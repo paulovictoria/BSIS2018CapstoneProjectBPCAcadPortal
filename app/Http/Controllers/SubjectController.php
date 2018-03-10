2cb0f2de57260ed4128386a2107fa6d4b0c0ce2c
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-09 20:02:13 -0800

    a

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -110,7 +110,9 @@ class SubjectController extends Controller
     public function allSubjects()
     {   
       $subjects=Subject::select('id','subj_code','subj_description','subj_units','sem','year');
-      return Datatables::of($subjects)->make(true);
+      return Datatables::of($subjects)->addColumn('action', function($subject){
+      return '<a href="#" class="btn edit" id="'.$subject->id.'"><i class="icon-pencil"></i>Edit</a>';
+      })->make(true);
     }
 
 
