23a46d9b8075477a24b66dc4b63f09c2eae42b14
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-09 18:30:08 -0800

    back my recent

--- app/Http/Controllers/ProfessorDashboardController.php
+++ app/Http/Controllers/ProfessorDashboardController.php
@@ -108,7 +108,19 @@ class ProfessorDashboardController extends Controller
 	   	'student_id'=>'required',
 	   	'subject_id'=>'required'
         );
-   
+        $validator = Validator::make (input::all(), $rules);
+        if($validator->fails())
+            return response::json(array('errors'=> $validator->getMessageBag()->toarray()));
+        
+        else {
+		   $grade=new Grade();
+		   $grade->grade=$request->grade;
+		   $grade->student_id=$request->student_id;
+		   $grade->subject_id=$request->subject_id;
+		   $grade->assign_id=$request->assign_id;
+		   $grade->save();
+            return response()->json($grade);
+        }
     }
 
 
