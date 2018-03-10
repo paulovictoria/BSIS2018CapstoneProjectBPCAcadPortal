01d2f0fe82dce69f3812db97e1efc39a93ff3a79
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-09 18:41:22 -0800

    cdn

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" href="{{ asset('css/materialize.css?v=' . env('ASSET_VERSION', 1)) }}">
+<!-- <link rel="stylesheet" href="{{ asset('css/materialize.css?v=' . env('ASSET_VERSION', 1)) }}"> -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 <link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('css/adminStyle.css') }}">
