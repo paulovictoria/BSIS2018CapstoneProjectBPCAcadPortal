eae19769214206fb31570c0b767a013605c965d1
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-09 19:39:16 -0800

    a

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -113,7 +113,7 @@ class SubjectController extends Controller
       return Datatables::
       of($subjects)->addColumn('action', function($subject){
         return '<a href="#" class="btn " id="'.$subject->id.'">
-                <i class="icon-pencil"></i>Edit</>'
+                <i class="icon-pencil"></i>Edit</a>'
       })->make(true);
     }
 
