f3bdc5c581b4502bd17e14360153a9189bd54287
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-10 00:39:47 -0800

    fix widdth

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -1,4 +1,9 @@
 <!-- <link rel="stylesheet" href="{{ asset('css/materialize.css?v=' . env('ASSET_VERSION', 1)) }}"> -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="theme-color" content="#ffffff">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 <link rel="stylesheet" href="{{ asset('css/icomoon.css') }}">
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
