7b5464faa0edc4f4ebfd2b0e98d49eb394030bea
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-09 20:34:32 -0800

    a

--- resources/views/professor/individualClassroom.blade.php
+++ resources/views/professor/individualClassroom.blade.php
@@ -151,7 +151,7 @@ $(document).on('click','#editButton', function(event){
 $('#updateGrade').click(function(event){
   $.ajax({
     type: "POST",
-    url: "{{route('addGrade.Student')}}",
+    url: "{{route('/addGrade.Student')}}",
     data: {
       '_token': $('input[name=_token]').val(),
       'id':$('#assignStudent').val(),
