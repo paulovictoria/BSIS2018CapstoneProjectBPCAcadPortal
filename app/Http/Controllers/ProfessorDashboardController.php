d93885af1dac28a93c9f33a6ca9daf14a5dd9c31
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-16 22:15:02 -0700

    day

--- app/Http/Controllers/ProfessorDashboardController.php
+++ app/Http/Controllers/ProfessorDashboardController.php
@@ -198,8 +198,8 @@ class ProfessorDashboardController extends Controller
 
     public function schedule() {
         $assigns=Assign::where('professor_id','=',Auth::user()->id)
- /*       ->join('days','assigns.day','=','days.id')*/
-        ->orderBy('day','asc')
+        ->join('days','assigns.day','=','days.id')
+
         ->get();
         return view('professor.schedule')->withAssigns($assigns);
     }
