c0aaf225ee98a3a4423f9d61c4fc82e19794270a
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-03-18 18:24:50 -0700

    another

--- resources/views/admin/approvalIndex.blade.php
+++ resources/views/admin/approvalIndex.blade.php
@@ -3,15 +3,14 @@
 @section('content')
 <div class="col s12">			
 	<div class="section">
-		<div class="card z-depth-1">
+		<div class="card z-depth-4 light-green lighten-5">
 			<div class="card-content">
 				<div class="row">
 					<div class="col s9 offset-s3">
-						<h3 class="light-green-text darken-2 card-title">Request for Approval</h3>
 						<div class="section">
 							@if(Session::has('success'))
 								<div class="col s12 p">
-									<div class="light-green darken-2 white-text">
+									<div class="yellow darken-4 white-text">
 										<p class="flow-text">{{ Session::get('success')}}</p>
 									</div>
 								</div>
@@ -19,11 +18,9 @@
 						</div>
 					<table id="approvalIndex" class="table responsive-table" cellspacing="0">
 						<thead>
-							<tr>
+							<tr class="green darken-3 white-text">
 								<th>EID</th>
-								<th>Last Name</th>
-								<th>First Name</th>
-								<th>Midle Name</th>
+								<th>Name</th>
 								<th>Email</th>
 								<th>Image</th>
 								<th>Approved</th>
@@ -32,13 +29,13 @@
 						</thead>
 						<tbody>
 							@foreach($professors as $professor)
-							<tr>
-								<td>{{$professor->sid}}</td>
-								<td>{{$professor->last_name}}</td>
-								<td>{{$professor->first_name}}</td>
-								<td>{{$professor->midle_name}}</td>
+							<tr class="light-green lighten-5">
+								<td>{{$professor->eid}}</td>
+								<td>{{$professor->last_name}}
+								{{$professor->first_name}}
+								{{$professor->midle_name}}</td>
 								<td>{{$professor->email}}</td>
-								<td><a href="#!user"><img class="circle" src="{{route('user.image',['filename'=>$professor->sid. '-' .$professor->id. '.jpg'])}}" width="50"></a></td>
+								<td><a href="#!user"><img class="circle" src="{{route('user.image',['filename'=>$professor->filename])}}" width="50"></a></td>
 								
 									<td>{!! Form::model($professor,['route'=>['professorsApproved',$professor->id],'method'=>'PUT'])!!}
 									<button class="btn blue lighten-2"><span class="icon-check"></span></button>
