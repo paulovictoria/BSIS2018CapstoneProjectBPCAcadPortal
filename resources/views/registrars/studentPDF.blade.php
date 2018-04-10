9b584e6be4b503dab23521ea4d195d350d2ffc5b
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-04-10 13:41:00 -0700

    pdf issue

--- resources/views/registrars/studentPDF.blade.php
+++ resources/views/registrars/studentPDF.blade.php
@@ -2,26 +2,28 @@
 <html lang="en">
   <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-<style type="text/css">
-
-</style>
   </head>
   <body>
- 	<div class="row">
- 		<div class="col s6 m6">
- 			<table class="bordered">
- 				<thead>
- 					<tr>
- 						<td>SUBJECT CODE</td>
- 						<td>SUBJECT DESCRIPTION</td>
- 						<td>GRADE</td>
- 						<td>EVALUATION</td>
- 						<td>SEMESTER</td>
- 					</tr>
- 				</thead>
- 				<tbody>
- 					@foreach($records as $record)
- 					 @if($record->year=="First Year")
+    <div class="section green darken-3 white-text center-align">
+        <h5>Bulacan Polytechnic College</h5>
+        <h6>First Year</h6>
+    </div>
+    <div class="divider"></div>
+    <div class="card grey lighten-2">
+        <div class="card-content">
+            <table class="table bordered striped center-align">
+                <thead>
+                    <tr>
+                        <td>SUBJECT CODE</td>
+                        <td>SUBJECT DESCRIPTION</td>
+                        <td>GRADE</td>
+                        <td>EVALUATION</td>
+                        <td>SEMESTER</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($records as $record)
+                     @if($record->year=="First Year")
                                             <tr>
                                                 <td>{{$record->subj_code}}</td>
                                                 <td>{{$record->subj_description}}</td>
@@ -43,25 +45,32 @@
                                                 @endif
                                                 <td>{{$record->sem}}</td>                 
                                             </tr> 
- 					@endif
- 					@endforeach
- 				</tbody>
- 			</table>
- 		</div>
- 		<div class="col s6 m6">
- 			<table class="bordered">
- 				<thead>
- 					<tr>
- 						<td>SUBJECT CODE</td>
- 						<td>SUBJECT DESCRIPTION</td>
- 						<td>GRADE</td>
- 						<td>EVALUATION</td>
- 						<td>SEMESTER</td>
- 					</tr>
- 				</thead>
- 				<tbody>
- 					@foreach($records as $record)
- 					 @if($record->year=="Second Year")
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="section green darken-3 white-text center-align">
+        <h5>Bulacan Polytechnic College</h5>
+        <h6>Second Year</h6>
+    </div>
+    <div class="divider"></div>
+    <div class="card grey lighten-2">
+        <div class="card-content">
+            <table class="bordered">
+                <thead>
+                    <tr>
+                        <td>SUBJECT CODE</td>
+                        <td>SUBJECT DESCRIPTION</td>
+                        <td>GRADE</td>
+                        <td>EVALUATION</td>
+                        <td>SEMESTER</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($records as $record)
+                     @if($record->year=="Second Year")
                                             <tr>
                                                 <td>{{$record->subj_code}}</td>
                                                 <td>{{$record->subj_description}}</td>
@@ -83,25 +92,32 @@
                                                 @endif
                                                 <td>{{$record->sem}}</td>                 
                                             </tr> 
- 					@endif
- 					@endforeach
- 				</tbody>
- 			</table>
- 		</div>
- 		<div class="col s6 m6">
- 			<table class="bordered">
- 				<thead>
- 					<tr>
- 						<td>SUBJECT CODE</td>
- 						<td>SUBJECT DESCRIPTION</td>
- 						<td>GRADE</td>
- 						<td>EVALUATION</td>
- 						<td>SEMESTER</td>
- 					</tr>
- 				</thead>
- 				<tbody>
- 					@foreach($records as $record)
- 					 @if($record->year=="Third Year")
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+     <div class="section green darken-3 white-text center-align">
+        <h5>Bulacan Polytechnic College</h5>
+        <h6>Third Year</h6>
+    </div>
+    <div class="divider"></div>
+    <div class="card grey lighten-2">
+        <div class="card-content">
+            <table class="bordered">
+                <thead>
+                    <tr>
+                        <td>SUBJECT CODE</td>
+                        <td>SUBJECT DESCRIPTION</td>
+                        <td>GRADE</td>
+                        <td>EVALUATION</td>
+                        <td>SEMESTER</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($records as $record)
+                     @if($record->year=="Third Year")
                                             <tr>
                                                 <td>{{$record->subj_code}}</td>
                                                 <td>{{$record->subj_description}}</td>
@@ -123,25 +139,32 @@
                                                 @endif
                                                 <td>{{$record->sem}}</td>                 
                                             </tr> 
- 					@endif
- 					@endforeach
- 				</tbody>
- 			</table>
- 		</div>
- 		<div class="col s6 m6">
- 			<table class="bordered">
- 				<thead>
- 					<tr>
- 						<td>SUBJECT CODE</td>
- 						<td>SUBJECT DESCRIPTION</td>
- 						<td>GRADE</td>
- 						<td>EVALUATION</td>
- 						<td>SEMESTER</td>
- 					</tr>
- 				</thead>
- 				<tbody>
- 					@foreach($records as $record)
- 					 @if($record->year=="Fourth Year")
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+     <div class="section green darken-3 white-text center-align">
+        <h5>Bulacan Polytechnic College</h5>
+        <h6>Fourth Year</h6>
+    </div>
+    <div class="divider"></div>
+    <div class="card grey lighten-2">
+        <div class="card-content">
+            <table class="bordered">
+                <thead>
+                    <tr>
+                        <td>SUBJECT CODE</td>
+                        <td>SUBJECT DESCRIPTION</td>
+                        <td>GRADE</td>
+                        <td>EVALUATION</td>
+                        <td>SEMESTER</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($records as $record)
+                     @if($record->year=="Fourth Year")
                                             <tr>
                                                 <td>{{$record->subj_code}}</td>
                                                 <td>{{$record->subj_description}}</td>
@@ -163,14 +186,17 @@
                                                 @endif
                                                 <td>{{$record->sem}}</td>                 
                                             </tr> 
- 					@endif
- 					@endforeach
- 				</tbody>
- 			</table>
- 		</div> 		 		 		 		 		
- 	</div>
+                    @endif
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>      
 
- <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+   <!--  <script src="{{ asset('jquery/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/materialize.js') }}"></script> -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
   </body>
 </html>
