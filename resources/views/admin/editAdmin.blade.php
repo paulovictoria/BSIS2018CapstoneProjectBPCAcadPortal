db4f6c16732a897e160a198d9482c1c5e411c25c
michaelpastor <michaelcentenovelayo@yahoo.com>
2018-04-09 17:29:28 -0700

    button design

--- resources/views/admin/editAdmin.blade.php
+++ resources/views/admin/editAdmin.blade.php
@@ -19,7 +19,7 @@
 							{{ Form::label('password','New Password') }}
 							{{ Form::text('newpassword',null,['class'=>'form-control']) }}	
 
-							{{Form::submit('save',['class'=>'btn  green darken-4 right-align'])}}
+							{{Form::submit('save',['class'=>'btn  green darken-4 right'])}}
 
 							{!! Form::close() !!}
 						</div>
